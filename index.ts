56542f9f97a927f0f01297489c5d9554309057cf
ShawnAnarchy <shawn.anarchy@protonmail.com>
2020-10-21 22:40:08 +0900

    update citizen and proposal

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as Random from 'random.ts'
+
 type validationResult = {
   code: string,
   report: string
@@ -9,6 +11,7 @@ interface ClockInterface {
 
 class StateMachine implements ClockInterface {
   people: Array<Citizen>;
+  treasury: number;
   annualRevenue: number;
   annualExpense: number;
   annualSeigniorage: number;
@@ -28,6 +31,13 @@ class StateMachine implements ClockInterface {
 
 
   constructor(){
+  }
+
+  payTax(amount){
+    this.treasury += amount;
+  }
+  withdrawWelfare(amount){
+    this.treasury -= amount;
   }
 
   getPopulation(){
@@ -70,7 +80,13 @@ class StateMachine implements ClockInterface {
       report: ""
     }
   }
-
+  submitProposal(proposer){
+    this.proposals.push(new Proposal(proposer, this))
+  }
+  destroyProposal(proposalId){
+    let index = this.proposals.map((p,i)=> p.id === proposalId ? i : 0).reduce(s,i=> s+i, 0)
+    this.proposals.splice(this.proposals, 1)
+  }
 }
 
 const enum ProblemTypes {
@@ -82,10 +98,26 @@ const enum ProblemTypes {
 }
 const enum ProposalPhasees {
   INITIAL_JUDGE = 'i',
+  FACILITATOR_ASSIGNMENT = 'fa',
+  DOMAIN_ASSIGNMENT = 'da',
+  PROFESSIONAL_ASSIGNMENT = 'pa',
+  DELIBERATION = 'd',
   FINAL_JUDGE = 'f',
   HEADCOUNT_EXCEEDED = 'h'
 }
+const enum Domains {
+  EARTH = 'e',
+  FIRE = 'f',
+  THUNDER = 't',
+  WIND = 'w',
+  LEAF = 'l'
+}
 class Proposal implements ClockInterface {
+  s: StateMachine;
+  proposer: Citizen;
+  facilitator?: Citizen;
+  professionals: Array<Citizen>;
+  domains: Array<Domains>
   problemType: ProblemTypes;
   durationDays:number;
   spentDays:number;
@@ -94,12 +126,17 @@ class Proposal implements ClockInterface {
   progressismDegree: number;
   humanrightsDegree: number;
 
-  constructor(){
+  constructor(proposer, s){
+    this.s = s
+    this.proposer = proposer;
+    this.facilitator = null;
+    this.domains = [];
+    this.professionals = [];
     this.durationDays = this.getDurationDays()
     this.spentDays = 0;
     this.representativeHeadcount = 30;
-    this.progressismDegree = 30 + rand(60)
-    this.humanrightsDegree = 30 + rand(60)
+    this.progressismDegree = 30 + Random.number(0, 60)
+    this.humanrightsDegree = 30 + Random.number(0, 60)
     this.administrationToBeCreated = new Administration()
     this.vestedMonthlyBudget = this.administrationToBeCreated.monthlyBudget
   }
@@ -167,18 +204,45 @@ class Proposal implements ClockInterface {
     }
     //TODO check duration and spentDays 
   }
+  initialJudge(){
+    if(this.status === ProposalPhasees.INITIAL_JUDGE){
+      if(this.proposer.IQ > 50) {
+        this.status = ProposalPhasees.FACILITATOR_ASSIGNMENT
+        return;
+      } else {
+        this.s.destroyProposal(this.id)
+      }
+    } else {
+      console.error('Proposal.initialJudge: wrong phase of proposal')
+    }
+  }
+  pickFacilitator(){
+    let availableFacilitators = this.s.facilitators.filter(f=> !f.isBusy )
+    this.facilitator = availableFacilitators[Random.number(0, availableFacilitators.length-1)]
+  }
+  pickDomains(){
+    this.domains = domains;
+  }
+  pickProfessionals(){
+    this.s.pickProfessionals(this)
+  }
+  deliberation(){
+    this.progressismDegree += Random.number(0, 10) - Random.number(0, 5)
+    this.humanrightsDegree += Random.number(0, 10) - Random.number(0, 5)
+  }
+  finalJudge(){
+
+  }
 }
 
 enum LifeStage {
   SUFFRAGE = 's',
   WORKFORCE = 'w',
-  MARIAGE = 'm',
-  CAN_BE_PREGNANT = 'g',
-  ELDERLY = 'e',
   NURSING = 'n',
   DEATH = 'd'
 }
 class Citizen implements ClockInterface {
+  s: StateMachine;
   annualSalary: number;
   intelligence: number;
   isSocioPath: bool;
@@ -186,62 +250,154 @@ class Citizen implements ClockInterface {
   biologicallyCanBePregnant: bool; 
   lifetime: number;
 
-  constructor(){
+  constructor(s){
+    this.s = s;
     this.annualSalary = 0;
-    this.IQ = 30 + rand(60);
-    this.ConspiracyPreference = 100 - this.IQ + rand(10) - rand(10);
-    this.CultPreference = 100 - this.IQ + rand(10) - rand(10);
-    this.isSocioPath = !!rand(1);
+    this.IQ = 30 + Random.number(0, 60);
+    this.ConspiracyPreference = 100 - this.IQ + Random.number(0, 10) - Random.number(0, 10);
+    this.CultPreference = 100 - this.IQ + Random.number(0, 10) - Random.number(0, 10);
+    this.isSocioPath = !!Random.number(0, 1);
     this.progressismPreference = 50*((this.IQ*this.ConspiracyPreference*this.CultPreference)/(50*50*50))^(1/3);
     this.humanrightsPreference = this.isSocioPath ? 30 :
       (this.progressismPreference > 60) ? 40 :
       (this.progressismPreference > 50) ? 50 :
       (this.progressismPreference > 40) ? 60 : 70;
-    this.biologicallyCanBePregnant = !!rand(1)
+    this.biologicallyCanBePregnant = !!Random.number(0, 1)
   }
   tick(){
     this.age += 3/365
     let validationResult = this.validate();
 
-    //TODO random active political action
-    //TODO random passive political action
-    //TODO pay tax
-    //TODO get welfare
-
-    switch(validationResult.code){
-      case LifeStage.SUFFRAGE:
-        return;
-      case LifeStage.WORKFORCE:
-        this.annualSalary += 2000
-        //TODO How we should tie the life stage and salary
-        return;
-      case LifeStage.MARIAGE:
-        return;
-      case LifeStage.CAN_BE_PREGNANT:
-        return;
-      case LifeStage.NURSING:
-        return;
-      case LifeStage.DEATH:
-        return;
-    }
+    this.earn(validationResult)
+    this.payTax(validationResult)
+    this.getWelfare(validationResult)
+    this.activePoliticalAction(validationResult)
+    this.passivePoliticalAction(validationResult)
   }
   validate(){
     if (this.age > this.lifetime) {
       return { code: LifeStage.DEATH, report: "" }
     } else if (this.age < 16 || 75 < this.age) {
       return { code: LifeStage.NURSING, report: "" }
-    } else if (this.age === 16) {
-      return { code: LifeStage.SUFFRAGE, report: "" }
-    } else if (this.age > 22) {
+    } else if (22 <= this.age && this.age <= 75) {
       return { code: LifeStage.WORKFORCE, report: "" }
-    } else if (this.age > 65) {
-      return { code: LifeStage.ELDERLY, report: "" }
-    } else if (14 < this.age && this.age < 50 && biologicallyCanBePregnant) {
-      return { code: LifeStage.CAN_BE_PREGNANT, report: "" }
+    } else if (16 <= this.age) {
+      return { code: LifeStage.SUFFRAGE, report: "" }
     }
     //TODO give birth as its tendency of giving birth
     //TODO political status
     //TODO social status
+  }
+  earn(validationResult){
+    switch(validationResult.code){
+      case LifeStage.SUFFRAGE:
+        this.annualSalary = -1500*12
+        return;
+      case LifeStage.WORKFORCE:
+        if (this.annualSalary < 2000*12) {
+          this.annualSalary = 2000*12 + Random.number(0, 1000*12)
+        } else if (65 < this.age) {
+          this.annualSalary = 3000*12 + Random.number(0, 2000*12) - Random.number(0, 2000*12)
+        } else if (50 < this.age) {
+          this.annualSalary = 5000*12 + Random.number(0, 5000*12) - Random.number(0, 1000*12)
+        } else if (46 < this.age) {
+          this.annualSalary = 5000*12 + Random.number(0, 4000*12) - Random.number(0, 2000*12)
+        } else if (32 < this.age) {
+          this.annualSalary = 4000*12 + Random.number(0, 4000*12) - Random.number(0, 1500*12)
+        } else if (28 < this.age) {
+          this.annualSalary = 3000*12 + Random.number(0, 2000*12) - Random.number(0, 1000*12)
+        }
+        return;
+      case LifeStage.NURSING:
+        this.annualSalary = -1000*12
+        return;
+      case LifeStage.DEATH:
+        this.annualSalary = 0
+        return;
+    }
+  }
+  payTax(validationResult){
+    let taxRate;
+    switch(validationResult.code){
+      case LifeStage.SUFFRAGE:
+        taxRate = 0;
+        return;
+      case LifeStage.WORKFORCE:
+        if (this.annualSalary < 2000*12) {
+          let taxRate = 0.1;
+        } else if (2000*12 <= this.annualSalary && this.annualSalary < 3000*12) {
+          let taxRate = 0.13;
+        } else if (3000*12 <= this.annualSalary && this.annualSalary < 4000*12) {
+          let taxRate = 0.15;
+        } else if (4000*12 <= this.annualSalary && this.annualSalary < 5000*12) {
+          let taxRate = 0.17;
+        } else if (5000*12 <= this.annualSalary && this.annualSalary < 6000*12) {
+          let taxRate = 0.19;
+        } else if (6000*12 <= this.annualSalary && this.annualSalary < 7000*12) {
+          let taxRate = 0.21;
+        } else if (7000*12 <= this.annualSalary && this.annualSalary < 8000*12) {
+          let taxRate = 0.23;
+        } else if (8000*12 <= this.annualSalary && this.annualSalary < 9000*12) {
+          let taxRate = 0.25;
+        } else if (9000*12 <= this.annualSalary && this.annualSalary < 10000*12) {
+          let taxRate = 0.27;
+        } else {
+          let taxRate = 0.38;
+        }
+      return;
+      case LifeStage.NURSING:
+        let taxRate = 0;
+        return;
+      case LifeStage.DEATH:
+        let taxRate = 0;
+        return;
+
+      this.annualSalary = this.annualSalary*(1-taxRate)
+      this.s.payTax(this.annualSalary*taxRate)
+    }
+  }
+  getWelfare(validationResult){
+    let welfareAmount;
+    switch(validationResult.code){
+      case LifeStage.SUFFRAGE:
+        welfareAmount = 1000*12
+        return;
+      case LifeStage.WORKFORCE:
+        welfareAmount = 100*12
+      return;
+      case LifeStage.NURSING:
+        welfareAmount = 2000*12
+        return;
+      case LifeStage.DEATH:
+        welfareAmount = 0
+        return;
+
+      this.annualSalary = this.annualSalary + welfareAmount
+      this.s.withdrawWelfare(welfareAmount)
+    }
+  }
+  activePoliticalAction(validationResult){
+    switch(validationResult.code){
+      case LifeStage.SUFFRAGE:
+      case LifeStage.WORKFORCE:
+      case LifeStage.NURSING:
+        if(this.age < 16){
+        } else {
+          if(this.annualSalary/12 > 5000 || this.IQ > 55) {
+            if(Random.number(0, 365/3) === 0){
+              this.s.submitProposal(this)
+              //TODO new Proposal(this)
+              //TODO IQ of proposer is important for the initial judge
+            } else {}
+          } else {}
+        } else {}
+        return;
+      case LifeStage.DEATH:
+        return;
+    }
+  }
+  passivePoliticalAction(validationResult){
+
   }
 }
 
@@ -281,16 +437,16 @@ class Administration implements ClockInterface {
   headCount: number;
 
   constructor(){
-    this.id = rand(1000000000000);
-    this.headCount = 8 + rand(6);
+    this.id = Random.number(0, 1000000000000);
+    this.headCount = 8 + Random.number(0, 6);
     this.monthlyBudget = this.headCount
-      .map(_=> 3000 + rand(2000) )
+      .map(_=> 3000 + Random.number(0, 2000) )
       .reduce((sum, el) => sum + el, 0);
-    this.curruption = rand(30);
+    this.curruption = Random.number(0, 30);
   }
 
   tick(){
-    this.curruption += rand(3)/10
+    this.curruption += Random.number(0, 3)/10
   }
 
 }
